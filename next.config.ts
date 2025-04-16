4ec0690399a8824fac1b13415c5faa679abd7c24
SvitlanaRamanauskas <ramanauskassvs@gmail.com>
2025-04-16 20:53:59 +0300

    webpack config deleted

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
